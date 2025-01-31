8a62963e012390c72eb309304a111fd5e6e5c95a
Adeniyi <adekunleabioye27@gmail.com>
2025-01-31 18:13:04 +0100

    initial commit

---
+++ hng/hng.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, status
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class EmailTimestampResponse(BaseModel):
+    email: str
+    timestamp: str
+    github: str
+    
+app = FastAPI()
+
+@app.get("/profile", response_model=EmailTimestampResponse, status_code=status.HTTP_200_OK)
+def get_email_and_timestamp():
+    return {
+        "email": "adekunleabioye27@gmail.com",
+        "current_datetime": datetime.utcnow().isoformat() + "Z",
+        "github_url" : "https://github.com/Lastknight78/hng_task_0"
+        }
